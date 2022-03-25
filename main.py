8e9e5573011c73f00a2ca28627e1ba7c2fd4a858
sunxin <sunxin@tetras.ai>
2022-03-25 19:14:24 +0800

    update

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ import sys
 
 
 def extract_keywords_from_docx():
-    file = docx.Document("keywords.docx")
+    file = docx.Document("keywords2.0.docx")
 
     keywords = []  # 国家，公司，党，其他
     k = 0
@@ -40,8 +40,8 @@ print(keywords[2])
 print(keywords[3])
 print()
 
-data = np.array(pd.read_excel('community_news.xlsx', sheet_name='社区新闻原始数据'))
-results = np.array(pd.read_excel('community_news.xlsx', sheet_name='结果'))
+data = np.array(pd.read_excel('community_news3.0.xlsx', sheet_name='社区新闻原始数据'))
+results = np.array(pd.read_excel('community_news3.0.xlsx', sheet_name='结果'))
 
 d_communitiy_names = data[:, 2]
 d_communitiy_news = data[:, 5]
@@ -61,10 +61,11 @@ def check_keywords_in_news(keywords, news):
     return 0
 
 
-count = np.zeros((r_communitiy_names.shape[0], 9))
+count = np.zeros((r_communitiy_names.shape[0], 17))
 for i, r_communitiy_name in enumerate(r_communitiy_names):
     total_num = r_communitiy_newsnums[i]
     country_num, company_num, party_num, others_num = 0, 0, 0, 0
+    country_party_num, country_company_num, party_company_num, country_party_company_num = 0, 0, 0, 0
     for j, name in enumerate(d_communitiy_names):
         if name == r_communitiy_name:
             news = d_communitiy_news[j]
@@ -74,6 +75,12 @@ for i, r_communitiy_name in enumerate(r_communitiy_names):
             company_num = company_num + check_keywords_in_news(keywords[1], news)
             party_num = party_num + check_keywords_in_news(keywords[2], news)
             others_num = others_num + check_keywords_in_news(keywords[3], news)
+
+            country_party_num = country_party_num + (check_keywords_in_news(keywords[0], news) & check_keywords_in_news(keywords[2], news))
+            country_company_num = country_company_num + (check_keywords_in_news(keywords[0], news) & check_keywords_in_news(keywords[1], news))
+            party_company_num = party_company_num + (check_keywords_in_news(keywords[2], news) & check_keywords_in_news(keywords[1], news))
+            country_party_company_num = country_party_company_num + (check_keywords_in_news(keywords[0], news) & check_keywords_in_news(keywords[1], news) & check_keywords_in_news(keywords[2], news))
+
     print('name:{}, total_num:{}, country_num:{}, company_num:{}, party_num:{}, others_num:{}'.format(r_communitiy_name,
                                                                                                       total_num,
                                                                                                       country_num,
@@ -89,6 +96,15 @@ for i, r_communitiy_name in enumerate(r_communitiy_names):
     count[i, 6] = party_num / total_num
     count[i, 7] = others_num
     count[i, 8] = others_num / total_num
+
+    count[i, 9]  = country_party_num
+    count[i, 10] = country_party_num / total_num
+    count[i, 11] = country_company_num
+    count[i, 12] = country_company_num / total_num
+    count[i, 13] = party_company_num
+    count[i, 14] = party_company_num / total_num
+    count[i, 15] = country_party_company_num
+    count[i, 16] = country_party_company_num / total_num
 print('完成统计')
 print()
 
